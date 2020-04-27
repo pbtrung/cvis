7d0dc2606acbd8f0b3b74acf398c797251871e61
Trung Pham <phambaotrung@gmail.com>
2020-04-27 17:37:38 -0400

    Update figures

--- cvis_rs_tols.m
+++ cvis_rs_tols.m
@@ -37,6 +37,7 @@ function cvis_rs_tols()
     min_ve_v0(1:length(tol)) = 0;
     astar(1:length(tol)) = 0;
     kldiv(1:length(tol)) = 0;
+    covar(1:length(tol)) = 0;
     nkl = 10000000;
     
     for r = 1:length(tol)
@@ -77,10 +78,11 @@ function cvis_rs_tols()
         display('prob0./me')
         prob0./me'
 
-        covar = cov(wQ0s,wQ1s);
-        v0 = covar(1,1);
-        v1 = covar(2,2);
-        ve = v0+a.^2*v1+2*a*covar(1,2);
+        co = cov(wQ0s,wQ1s);
+        v0 = co(1,1);
+        v1 = co(2,2);
+        covar(r) = co(1,2)
+        ve = v0+a.^2*v1+2*a*covar(r);
 
         display('v./v0')
         v/v0
@@ -89,19 +91,55 @@ function cvis_rs_tols()
         display('ve./v0')
         ve'./v0
         
-        astar(r) = -covar(1,2)/v1;
+        astar(r) = -covar(r)/v1;
         astar(2:end)
-        min_ve_v0(r) = (v0+astar(r)^2*v1+2*astar(r)*covar(1,2))/v0
+        min_ve_v0(r) = (v0+astar(r)^2*v1+2*astar(r)*covar(r))/v0
         
         kldiv(r) = kl(nkl,umin,umax,q1,q2)
     
     end
     
+    set(0,'defaultLineLineWidth',0.7);
+    set(0,'defaultLineMarkerSize',2);
+    fontsize = 8;
+    width = 3;
+    height = 3;
+    
+    figs(1) = figure('Units','inches',...
+        'Position',[0 0 width height],...
+        'visible','off',...
+        'PaperPositionMode','auto');
     hold on
-    plot(l1-tol,min_ve_v0,'-o',l1-tol,kldiv,'--*')
-    legend('min(v_e/v_0)','KL Divergence')
-    xlabel('Threshold')
+    plot(l1-tol,min_ve_v0,'-o',l1-tol,kldiv,'-o',l1-tol,covar*10^7,'-o')
+    set(gca,...
+        'Units','normalized',...
+        'FontUnits','points',...
+        'FontWeight','normal',...
+        'FontSize',fontsize,...
+        'FontName','Times')
+    legend({'$\min(v_e/v_0)$','KL Divergence','Covariance$\times 10^7$'},...
+        'interpreter','latex',...
+        'FontSize',fontsize,...
+        'FontName','Times',...
+        'Location','NorthEast')
+    ylabel('',...
+        'FontUnits','points',...
+        'interpreter','latex',...
+        'FontSize',fontsize,...
+        'FontName','Times')
+    xlabel({'Threshold'},...
+        'FontUnits','points',...
+        'interpreter','latex',...
+        'FontWeight','normal',...
+        'FontSize',fontsize,...
+        'FontName','Times')
+    fn(1) = "tol_min.eps";
     hold off
+    
+    for k = 1:length(figs)
+        % print each figure in figs to a separate .eps file
+        print(figs(k), '-depsc2', sprintf('%s', fn(k)))
+    end
     
 end
 
