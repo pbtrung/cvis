c6318b91929ea427891477ebae2f40e41f1cc876
Trung Pham <pham.bao.trung@gmail.com>
2020-08-03 15:21:15 -0400

    Update bench

--- bench.m
+++ bench.m
@@ -15,45 +15,56 @@ function bench()
     upper = 2;
     
     [Psi,lambda,PsiE1] = KL(lx,ly,Lx,Ly,nelx,nely,2);
-%     outer = 1000;
-%     inner = 1000;
-%     EQ_EQ1(lx,ly,nelx,nely,nelx1,nely1,outer,inner,Psi,lambda,PsiE1,lower,upper);
+    outer = 1;
+    inner = 100;
+    EQ_EQ1(lx,ly,nelx,nely,nelx1,nely1,outer,inner,Psi,lambda,PsiE1,lower,upper);
 
+end
+
+function EQ_EQ1(lx,ly,nelx,nely,nelx1,nely1,outer,inner,Psi,lambda,PsiE1,a,b)
+
+    force = 1;
+    Uy(1:outer,1:inner) = 0;
+    dof = 2*(nely+1)*nelx+2;
+    
     neig = length(lambda);
     L = diag(sqrt(lambda(1:neig)));
     P = reshape(permute(Psi,[2 1 3]),nelx*nely,neig);
-
-    l0 = 118.923;
-    l1 = 108.510;
-    
-    dof0 = 2*(nely+1)*nelx+2;
-    dof1 = 2*(nely1+1)*nelx1+2;
-    force = 1;
-    
-    Q0 = @(x) l0-FEQ0(lx,ly,nelx,nely,dof0,force,x,P,L,lower,upper)';
-    Q1 = @(x) l1-FEQ1(lx,ly,nelx1,nely1,dof1,force,x,PsiE1,L,lower,upper)';
+    E(1:nely,1:nelx,1:inner) = 0;
         
-    K = 100;
-    nsamples = 100;
-    
-    % definition of the random variables
-    d      = neig;
-    pi_pdf = repmat(ERADist('standardnormal','PAR'),d,1);
-    
-    % CE method
-    N      = 5000;    % total number of samples for each level
-    p      = 0.1;     % quantile value to select samples for parameter update
-    k_init = 5;       % initial number of distributions in the Mixture models (GM/vMFNM)
-     
-    % limit state function
-    g = @(x) Q1(x);
-    [~,~,~,~,~,~,~,mu_hat,Si_hat,Pi_hat] = CEIS_GM(N,p,g,pi_pdf,k_init);
-    gm = gmdistribution(mu_hat,Si_hat,Pi_hat);
-    
-    for j = 1:2
-        size(random(gm,nsamples))
-%         Q0s = Q0(samples')<0;
-%         Q1s = Q1(samples')<0;
+    for i = 1:outer
+        for j = 1:inner
+            Z = randn(neig,1);
+            LZ = L*Z;
+            PZ = dot(P',repmat(LZ,1,nelx*nely));
+            X = reshape(PZ,nelx,nely)';
+            E(:,:,j) = a+(b-a)*normcdf(X);
+        end
+        tic
+        for j = 1:inner
+            fprintf('Uy, outer: %d, inner: %d\n',i,j);
+            U = FErf(lx,ly,nelx,nely,dof,force,E(:,:,j));
+            Uy(i,j) = U(dof);
+        end
+        toc
     end
-
+    
+    Uy(1:outer,1:inner) = 0;
+    dof = 2*(nely1+1)*nelx1+2;
+    for i = 1:outer
+        E1(1:inner) = 0;
+        for j = 1:inner
+            Z = randn(neig,1);
+            X = PsiE1*(L*Z);
+            E1(j) = a+(b-a)*normcdf(X);
+        end
+        tic
+        for j = 1:inner
+            fprintf('Uy1, outer: %d, inner: %d\n',i,j);
+            U = FErf1(lx,ly,nelx1,nely1,dof,force,E1(j));
+            Uy(i,j) = U(dof);
+        end
+        toc
+    end
+    
 end
