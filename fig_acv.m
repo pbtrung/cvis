b9e9190340793f225e3ff4d63e5a4a98d50f9d7e
Trung Pham <pham.bao.trung@gmail.com>
2020-08-06 18:44:34 -0400

    Update fig

--- fig_acv.m
+++ fig_acv.m
@@ -1,28 +1,40 @@
-function fig_acv(path,p0,p1,p1_acv1,p1_acv2)
+function fig_acv(save_path,data_path,p_wQ0s,p_wQ0s_cv,p_wQ1s_cv,p_wQ0s_acv1,p_wQ1s_acv1,p_mu_acv1,p_wQ0s_acv2,p_wQ1s_acv2,p_mu_acv2)
 
-    wQ0s = readmatrix(p0);
-    wQ1s = readmatrix(p1);
-    wQ1s_acv1 = readmatrix(p1_acv1);
-    wQ1s_acv2 = readmatrix(p1_acv2);
+    wQ0s = readmatrix(append(data_path,p_wQ0s));
     
-    a = linspace(-1.5,0.5,33);
+    wQ0s_cv = readmatrix(append(data_path,p_wQ0s_cv));
+    wQ1s_cv = readmatrix(append(data_path,p_wQ1s_cv));
     
-    covar = cov(wQ0s,wQ1s);
-    covar_acv1 = cov(wQ1s,wQ1s_acv1);
-    covar_acv2 = cov(wQ1s,wQ1s_acv2);
-    covar_0acv2 = cov(wQ0s,wQ1s_acv2);
-    v0 = covar(1,1);
-    v1 = covar(2,2);
-    v_acv1 = covar_acv1(2,2);
-    v_acv2 = covar_acv2(2,2);
-    ve = v0 + a.^2*v1 + 2*a*covar(1,2);
-    ve_acv1 = v0 + a.^2*(v1+v_acv1) + 2*a*covar(1,2);
-    ve_acv2 = v0 + a.^2*(v1+v_acv2-2*covar_acv2(1,2)) + 2*a*(covar(1,2)-covar_0acv2(1,2));
+    wQ0s_acv1 = readmatrix(append(data_path,p_wQ0s_acv1));
+    wQ1s_acv1 = readmatrix(append(data_path,p_wQ1s_acv1));
+    mu_acv1 = readmatrix(append(data_path,p_mu_acv1));
     
-    covar(1,2)
-    covar_acv1(1,2)
-    covar_acv2(1,2)
-    covar_0acv2(1,2)
+    wQ0s_acv2 = readmatrix(append(data_path,p_wQ0s_acv2));
+    wQ1s_acv2 = readmatrix(append(data_path,p_wQ1s_acv2));
+    mu_acv2 = readmatrix(append(data_path,p_mu_acv2));
+    
+    a = linspace(-2.3,0.2,40);
+    
+    v0 = var(wQ0s);
+    cov_cv = cov(wQ0s_cv,wQ1s_cv);
+    v0_cv = cov_cv(1,1);
+    v1_cv = cov_cv(2,2);
+    ve = v0_cv + a.^2*v1_cv + 2*a*cov_cv(1,2);
+    
+    cov_acv1 = cov(wQ0s_acv1,wQ1s_acv1);
+    v0_acv1 = cov_acv1(1,1);
+    v1_acv1 = cov_acv1(2,2);
+    cov_Q1_mu_acv1 = cov(wQ1s_acv1,mu_acv1);
+    v_mu_acv1 = cov_Q1_mu_acv1(2,2);
+    ve_acv1 = v0_acv1 + a.^2*(v1_acv1+v_mu_acv1) + 2*a*cov_acv1(1,2);
+    
+    cov_acv2 = cov(wQ0s_acv2,wQ1s_acv2);
+    v0_acv2 = cov_acv2(1,1);
+    v1_acv2 = cov_acv2(2,2);
+    cov_Q1_mu_acv2 = cov(wQ1s_acv2,mu_acv2);
+    v_mu_acv2 = cov_Q1_mu_acv2(2,2);
+    cov_0acv2 = cov(wQ0s_acv2,mu_acv2);
+    ve_acv2 = v0_acv2 + a.^2*(v1_acv2+v_mu_acv2-2*cov_Q1_mu_acv2(1,2)) + 2*a*(cov_acv2(1,2)-cov_0acv2(1,2));
         
     set(0,'defaultLineLineWidth',0.7);
     set(0,'defaultLineMarkerSize',2);
@@ -30,42 +42,15 @@ function fig_acv(path,p0,p1,p1_acv1,p1_acv2)
     width = 2.5;
     height = 2.5;
     
-%     ve'./v0
-%     ve_acv1'./v0
-%     ve_acv2'./v0
+    ve'./v0
+    ve_acv1'./v0
+    ve_acv2'./v0
+    
+    min(ve'./v0)
+    min(ve_acv1'./v0)
+    min(ve_acv2'./v0)
     
     figs(1) = figure('Units','inches',...
-        'Position',[0 0 width height],...
-        'visible','off',...
-        'PaperPositionMode','auto');
-    hold on
-    plot(a,ve,'-o',a,v0*ones(1,length(a)),'-o');
-    set(gca,...
-        'Units','normalized',...
-        'FontUnits','points',...
-        'FontWeight','normal',...
-        'FontSize',fontsize,...
-        'FontName','Times')
-    legend({'$v_e$','$v_0$'},...
-        'interpreter','latex',...
-        'FontSize',fontsize,...
-        'FontName','Times',...
-        'Location','NorthEast')
-    ylabel('Variance',...
-        'FontUnits','points',...
-        'interpreter','latex',...
-        'FontSize',fontsize,...
-        'FontName','Times')
-    xlabel({'$\alpha$'},...
-        'FontUnits','points',...
-        'interpreter','latex',...
-        'FontWeight','normal',...
-        'FontSize',fontsize,...
-        'FontName','Times')
-    fn(1) = "vev0.eps";
-    hold off
-    
-    figs(2) = figure('Units','inches',...
         'Position',[0 0 width height],...
         'visible','on',...
         'PaperPositionMode','auto');
@@ -93,43 +78,12 @@ function fig_acv(path,p0,p1,p1_acv1,p1_acv2)
         'FontWeight','normal',...
         'FontSize',fontsize,...
         'FontName','Times')
-    fn(2) = "ve_v0_acv.eps";
-    hold off
-    
-    figs(3) = figure('Units','inches',...
-        'Position',[0 0 width height],...
-        'visible','off',...
-        'PaperPositionMode','auto');
-    hold on
-    plot(a,log10(ve),'-o',a,log10(v0*ones(1,length(a))),'-o');
-    set(gca,...
-        'Units','normalized',...
-        'FontUnits','points',...
-        'FontWeight','normal',...
-        'FontSize',fontsize,...
-        'FontName','Times')
-    legend({'$\log_{10}(v_e)$','$\log_{10}(v_0)$'},...
-        'interpreter','latex',...
-        'FontSize',fontsize,...
-        'FontName','Times',...
-        'Location','NorthWest')
-    ylabel('Logarithm of Variance',...
-        'FontUnits','points',...
-        'interpreter','latex',...
-        'FontSize',fontsize,...
-        'FontName','Times')
-    xlabel({'$\alpha$'},...
-        'FontUnits','points',...
-        'interpreter','latex',...
-        'FontWeight','normal',...
-        'FontSize',fontsize,...
-        'FontName','Times')
-    fn(3) = "logvev0.eps";
+    fn(1) = "ve_v0_acv.eps";
     hold off
     
     for k = 1:length(figs)
         % print each figure in figs to a separate .eps file  
-        print(figs(k), '-depsc2', sprintf('%s', append(path,fn(k))))
+        print(figs(k), '-depsc2', sprintf('%s', append(save_path,fn(k))))
     end
     
 end
