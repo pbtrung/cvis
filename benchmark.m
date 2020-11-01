40c4cc8d7e62b74e8fa19acb8fb8bef9a79b8ad1
Trung Pham <pham.bao.trung@gmail.com>
2020-11-01 15:59:04 -0500

    Add benchmark

---
+++ benchmark.m
@@ -0,0 +1,14 @@
+function benchmark()
+    
+    format long;
+    rng('default');
+    
+    [filepath,~,~] = fileparts(matlab.desktop.editor.getActiveFilename);
+    repath = fullfile(filepath,'results');
+    
+    t0 = readmatrix(fullfile(repath,'t_20x20_midNode.txt'));
+    t1 = readmatrix(fullfile(repath,'t_10x10_midNode.txt'));
+    
+    disp(mean(t0)/mean(t1));
+    
+end
