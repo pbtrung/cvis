8dd3ade38b4f1f180e7cfc8fe5540942db8c55d3
Trung Pham <pham.bao.trung@gmail.com>
2020-11-01 15:51:13 -0500

    Add MC

---
+++ FE_plate.m
@@ -0,0 +1,48 @@
+function wmax = FE_plate(nelx,nely,E,poisson,kapa,L,x)
+    
+    % mesh generation
+    numberElements = nelx*nely;
+    %
+    [nodeCoordinates, elementNodes] = ...
+        rectangularMesh(L,L,nelx,nely,'Q4');
+    xx = nodeCoordinates(:,1);
+    yy = nodeCoordinates(:,2);
+
+    numberNodes = size(xx,1);
+    nn = flipud(reshape(1:numberElements,nely,nelx)');
+    nnn = mat2cell(nn,[nely/2 nelx/2],[nely/2 nelx/2]);
+    elemNum(1:4,(nelx/2)^2) = 0;
+    elemNum(1,:) = sort(nnn{2,1}(:));
+    elemNum(2,:) = sort(nnn{2,2}(:));
+    elemNum(3,:) = sort(nnn{1,1}(:));
+    elemNum(4,:) = sort(nnn{1,2}(:));
+    % GDof: global number of degrees of freedom
+    GDof = 3*numberNodes;
+    
+    nsamples = size(x,1);
+    midNode = (numberNodes+1)/2;
+    P = x(:,1:4);
+    h = x(:,5:8)/20;
+    
+    % boundary conditions
+    [prescribedDof,~] = ...
+        EssentialBC('cccc',GDof,xx,yy,nodeCoordinates,numberNodes);
+    
+    wmax(1:nsamples,1) = 0;
+    parfor i = 1:nsamples
+        % computation of the system stiffness matrix and force vector
+        stiffness = ...
+            formStiffnessMatrixMindlin_R(GDof,...
+            elementNodes,numberNodes,nodeCoordinates,...
+            'Q4','complete','reduced',E,poisson,kapa,h(i,:),elemNum);
+
+        force = ...
+            formForceVectorMindlin_R(GDof,...
+            elementNodes,nodeCoordinates,P(i,:),'Q4','reduced',elemNum);
+
+        % solution
+        displacements = solution(GDof,prescribedDof,stiffness,force);
+        wmax(i) = displacements(midNode);
+    end
+    
+end
---
+++ cvis_ce_mc.m
@@ -0,0 +1,94 @@
+function cvis_ce_mc()
+    
+    format long;
+    rng('default');
+    
+    %% materials
+    E = 10000;
+    poisson = 0.30;
+    kapa = 5/6;
+    L = 1;
+    
+    %%
+    nelx0 = 20;
+    nely0 = 20;
+    nelx1 = 10;
+    nely1 = 10;
+    if nelx0 ~= nely0 || mod(nelx0,2) ~= 0 || nelx1 ~= nely1 || mod(nelx1,2) ~= 0
+        error("Check inputs")
+    end
+    l0 = 1;
+    l1 = 1;
+    Q0 = @(x) l0-FE_plate(nelx0,nely0,E,poisson,kapa,L,x);
+    Q1 = @(x) l1-FE_plate(nelx1,nely1,E,poisson,kapa,L,x);
+    
+    %% definition of the random variables
+    d = 8;
+    lower = 1;
+    upper = 2;
+    pi_pdf = repmat(ERADist('uniform','PAR',[lower upper]),d,1);
+    if any(strcmp('Marginals',fieldnames(pi_pdf))) == 1
+        % use Nataf transform (dependence)
+        u2x = @(u) pi_pdf.U2X(u);
+    else
+        % use distribution information for the transformation (independence)
+        u2x = @(u) pi_pdf(1).icdf(normcdf(u));
+    end
+    
+    % CE method
+    N      = 5000;    % total number of samples for each level
+    p      = 0.1;     % quantile value to select samples for parameter update
+    k_init = 5;       % initial number of distributions in the Mixture models (GM/vMFNM)
+    
+    % limit state function
+    g = @(x) Q1(x');
+    [~,~,~,~,~,~,~,mu_hat,Si_hat,Pi_hat] = CEIS_GM(N,p,g,pi_pdf,k_init);
+    gm = gmdistribution(mu_hat,Si_hat,Pi_hat);
+    qce = @(x) pdf(gm,x);
+    
+    [filepath,~,~] = fileparts(matlab.desktop.editor.getActiveFilename);
+    repath = fullfile(filepath,'results');
+    
+    %%
+    n_MC = 5*10^5;
+    
+    s_MC = random(gm, n_MC);
+    writematrix(s_MC, fullfile(repath,'s_MC.txt'));
+    
+    w0_MC = mvnpdf(s_MC,mu,stdd)./qce(s_MC);
+    writematrix(w0_MC, fullfile(repath,'w0_MC.txt'));
+    w1_MC = mvnpdf(s_MC,mu,stdd)./qce(s_MC);
+    writematrix(w1_MC, fullfile(repath,'w1_MC.txt'));
+    
+    s_MC = u2x(s_MC);
+    
+    Q0s_MC = Q0(s_MC)<0;
+    writematrix(Q0s_MC, fullfile(repath,'Q0s_MC.txt'));
+    wQ0s_MC = w0_MC.*Q0s_MC;
+    writematrix(wQ0s_MC, fullfile(repath,'wQ0s_MC.txt'));
+    
+    Q1s_MC = Q1(s_MC)<0;
+    writematrix(Q1s_MC, fullfile(repath,'Q1s_MC.txt'));
+    wQ1s_MC = w1_MC.*Q1s_MC;
+    writematrix(wQ1s_MC, fullfile(repath,'wQ1s_MC.txt'));
+    
+    %%
+    m1_IS = 2*10^6;
+    
+    s_IS = random(gm, m1_IS);
+    writematrix(s_IS, fullfile(repath,'s_IS.txt'));
+    w1_IS = mvnpdf(s_IS,mu,stdd)./qce(s_IS);
+    writematrix(w1_IS, fullfile(repath,'w1_IS.txt'));
+    
+    s_IS = u2x(s_IS);
+    
+    Q1s_IS = Q1(s_IS)<0;
+    writematrix(Q1s_IS, fullfile(repath,'Q1s_IS.txt'));
+    wQ1s_IS = w1_IS.*Q1s_IS;
+    writematrix(wQ1s_IS, fullfile(repath,'wQ1s_IS.txt'));
+    
+    disp(mean(wQ0s_MC));
+    disp(mean(wQ1s_MC));
+    disp(mean(wQ1s_IS));
+    
+end
