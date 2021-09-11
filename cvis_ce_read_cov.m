48b011752f75de8e656db2947aabcbf719a90c50
Trung Pham <pham.bao.trung@gmail.com>
2021-09-11 14:46:39 -0400

    Add Pearson

---
+++ cvis_ce_read_cov.m
@@ -0,0 +1,15 @@
+function [cov_Q0Q1, rho] = cvis_ce_read_cov()
+    
+    format long;
+    
+    [filepath,~,~] = fileparts(matlab.desktop.editor.getActiveFilename);
+    repath = fullfile(filepath,'Example 3');
+
+    wQ0s_MC = readmatrix(fullfile(repath,'wQ0s_MC_cov.txt'));
+    wQ1s_MC = readmatrix(fullfile(repath,'wQ1s_MC_cov.txt'));
+    
+    cov_Q0Q1_MC = cov(wQ0s_MC,wQ1s_MC);
+    cov_Q0Q1 = cov_Q0Q1_MC(1,2);
+    rho = cov_Q0Q1_MC(1,2)/(cov_Q0Q1_MC(1,1)*cov_Q0Q1_MC(2,2));
+    
+end
