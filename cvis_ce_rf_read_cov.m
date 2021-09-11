5e561ee077e3a8541f200ad42d007878e829142a
Trung Pham <pham.bao.trung@gmail.com>
2021-09-11 14:53:56 -0400

    Fix Pearson

--- cvis_ce_rf_read_cov.m
+++ cvis_ce_rf_read_cov.m
@@ -9,7 +9,7 @@ function [cov_Q0Q1, rho] = cvis_ce_rf_read_cov()
     wQ1s_MC = readmatrix(fullfile(repath,'wQ1s_MC_cov.txt'));
     
     cov_Q0Q1_MC = cov(wQ0s_MC,wQ1s_MC);
-    cov_Q0Q1 = cov_Q0Q1_MC(1,2);
-    rho = cov_Q0Q1_MC(1,2)/(cov_Q0Q1_MC(1,1)*cov_Q0Q1_MC(2,2));
+    cov_Q0Q1 = cov_Q0Q1_MC(1,2)
+    rho = cov_Q0Q1_MC(1,2)/sqrt(cov_Q0Q1_MC(1,1)*cov_Q0Q1_MC(2,2))
     
 end
